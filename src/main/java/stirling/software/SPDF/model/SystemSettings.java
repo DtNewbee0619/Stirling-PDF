30d653d2938d8c36462e6a3918a1ea8a614b130f
Carpe-Wang <wangcarpe@gmail.com>
2025-04-21 12:50:15 -0400

    finished

---
+++ src/main/java/stirling/software/SPDF/model/SystemSettings.java
@@ -0,0 +1,10 @@
+package stirling.software.SPDF.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SystemSettings {
+    private Boolean disableSanitize;
+}
